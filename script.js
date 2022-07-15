411a5b5ec0610b408476237a68d0e510ba2ded6d
PhilippGraf11 <philippgepunkt@gmail.com>
2022-07-15 20:46:34 +0200

    computerauswahl fertig

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+// function getcomputerchoice
+// computer sagt schere stein oder papier random
+// spieler sagt schere stein oder papier via prompt
+// Computer Input
+
+// <0,34 Rock
+// <=0,67 Paper
+// ansonsten Scissors
+
+
+
+
+var computerChoice = Math.random();
+var userChoice = prompt("Make your choice")
+
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+}
+else   {
+    computerChoice = "scissors";
+}
+
+
